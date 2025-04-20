21962cf21912dbaa6a834417c86dd4500ab1cdf7
mmaheen <mushrifmaheen@gmail.com>
2025-04-20 19:43:48 +0600

    Prefix group route add

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::middleware('auth')->name('guest.')->group(function(){
     Route::get('/guest/dashboard',[App\Http\Controllers\GuestController::class, 'index'])->name('index');
 });
 
-Route::middleware(['auth',AuthAdmin::class])->name('admin.')->group(function(){
-    Route::get('admin/dashboard',[App\Http\Controllers\AdminController::class, 'index'])->name('index');
-    Route::resource('admin/brands',App\Http\Controllers\BrandController::class);
+Route::prefix('admin')->middleware(['auth',AuthAdmin::class])->name('admin.')->group(function(){
+    Route::get('/dashboard',[App\Http\Controllers\AdminController::class, 'index'])->name('index');
+    Route::resource('/brand',App\Http\Controllers\BrandController::class);
 });
